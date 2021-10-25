85f82359c5f84955c625f77703a412c95e9d8b63
AmateurHackerWillBreakTheGame <chengmixuexi@gmail.com>
2021-10-25 23:10:08 +1100

    update onLevelButtonsClick with markers

--- app/src/main/java/com/example/homepage/MapsActivity.kt
+++ app/src/main/java/com/example/homepage/MapsActivity.kt
@@ -1,35 +1,62 @@
 package com.example.homepage
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
-import android.widget.Button
 import android.widget.Toast
-
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.example.homepage.databinding.ActivityMapsBinding
+import com.example.homepage.model.Place
+import com.example.homepage.utils.BitmapHelper
+import com.example.homepage.utils.MarkerInfoWindowAdapter
+import com.example.homepage.utils.addressToLocation
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import com.example.homepage.databinding.ActivityMapsBinding
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.gms.maps.model.Marker
+
+
+
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private val  rotateOpen: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.rotate_open) }
     private val  rotateClose: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.rotate_close) }
     private val  fromBottom: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.from_bottom) }
     private val  toBottom: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.to_bottom) }
-    private var clicked = false;
+    private var clicked = false
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private val TAG = "MapsActivity"
 
     private lateinit var levelThreeBtn:View
     private lateinit var levelTwoBtn:View
     private lateinit var levelOneBtn:View
     private lateinit var layerButton:View
+
+    private var data = mockData()
+    var myMarkers = ArrayList<Marker>()
+
+    private val alertOneIcon: BitmapDescriptor by lazy {
+        val color = ContextCompat.getColor(this, R.color.red)
+        BitmapHelper.vectorToBitmap(this, R.drawable.tier1)
+    }
+
+    private val alertTwoIcon: BitmapDescriptor by lazy {
+        val color = ContextCompat.getColor(this, R.color.orange)
+        BitmapHelper.vectorToBitmap(this, R.drawable.tier2)
+    }
+
+    private val alertThreeIcon: BitmapDescriptor by lazy {
+        val color = ContextCompat.getColor(this, R.color.blue)
+        BitmapHelper.vectorToBitmap(this, R.drawable.tier3)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -40,6 +67,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        mapFragment.getMapAsync {
+            addMarkers()
+            mMap.setInfoWindowAdapter(MarkerInfoWindowAdapter(this))
+        }
 
         levelThreeBtn= findViewById(R.id.levelThreeBtn)
         levelTwoBtn= findViewById(R.id.levelTwoBtn)
@@ -51,12 +82,30 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         }
 
         levelOneBtn.setOnClickListener{
+            myMarkers.forEach { marker ->
+                var place = marker.tag as Place
+                if (place.alert_level == 1) {
+                    marker.isVisible = !marker.isVisible
+                }
+            }
             Toast.makeText(this, "levelOne Clicked",Toast.LENGTH_SHORT).show()
         }
         levelTwoBtn.setOnClickListener{
+            myMarkers.forEach { marker ->
+                var place = marker.tag as Place
+                if (place.alert_level == 2) {
+                    marker.isVisible = !marker.isVisible
+                }
+            }
             Toast.makeText(this, "levelTwo Clicked",Toast.LENGTH_SHORT).show()
         }
         levelThreeBtn.setOnClickListener{
+            myMarkers.forEach { marker ->
+                var place = marker.tag as Place
+                if (place.alert_level == 3) {
+                    marker.isVisible = !marker.isVisible
+                }
+            }
             Toast.makeText(this, "levelThree Clicked",Toast.LENGTH_SHORT).show()
         }
     }
@@ -70,13 +119,13 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private fun setVisibility(clicked: Boolean) {
 
         if(!clicked){
-            levelOneBtn.visibility = View.VISIBLE;
-            levelTwoBtn.visibility = View.VISIBLE;
-            levelThreeBtn.visibility = View.VISIBLE;
+            levelOneBtn.visibility = View.VISIBLE
+            levelTwoBtn.visibility = View.VISIBLE
+            levelThreeBtn.visibility = View.VISIBLE
         }else{
-            levelOneBtn.visibility = View.INVISIBLE;
-            levelTwoBtn.visibility = View.INVISIBLE;
-            levelThreeBtn.visibility = View.INVISIBLE;
+            levelOneBtn.visibility = View.INVISIBLE
+            levelTwoBtn.visibility = View.INVISIBLE
+            levelThreeBtn.visibility = View.INVISIBLE
         }
 
     }
@@ -84,13 +133,13 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private fun setAnimation(clicked: Boolean) {
 
         if(!clicked){
-            levelOneBtn.startAnimation(fromBottom);
-            levelTwoBtn.startAnimation(fromBottom);
+            levelOneBtn.startAnimation(fromBottom)
+            levelTwoBtn.startAnimation(fromBottom)
             levelThreeBtn.startAnimation(fromBottom)
             layerButton.startAnimation(rotateOpen)
         }else{
-            levelOneBtn.startAnimation(toBottom);
-            levelTwoBtn.startAnimation(toBottom);
+            levelOneBtn.startAnimation(toBottom)
+            levelTwoBtn.startAnimation(toBottom)
             levelThreeBtn.startAnimation(toBottom)
             layerButton.startAnimation(rotateClose)
         }
@@ -112,7 +161,123 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val melbourne = LatLng(-37.8116, 144.9646)
         mMap.addMarker(MarkerOptions().position(melbourne).title("Marker in Melbourne"))
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(melbourne,15f))
+    }
 
+    private fun addMarker(place: Place) {
+        when (place.alert_level) {
+            1 -> {
+                val marker = mMap.addMarker(
+                    MarkerOptions()
+                        .title(place.name)
+                        .position(place.latLng)
+                        .icon(alertOneIcon)
+                )
+                marker.tag = place
+            }
+            2 -> {
+                val marker = mMap.addMarker(
+                    MarkerOptions()
+                        .title(place.name)
+                        .position(place.latLng)
+                        .icon(alertTwoIcon)
+                )
+                marker.tag = place
+            }
+            3 -> {
+                val marker = mMap.addMarker(
+                    MarkerOptions()
+                        .title(place.name)
+                        .position(place.latLng)
+                        .icon(alertThreeIcon)
+                )
+                // Set place as the tag on the marker object so it can be referenced within
+                // MarkerInfoWindowAdapter
+                marker.tag = place
+            }
+        }
+    }
+
+    /**
+     * Adds marker representations of the places list on the provided GoogleMap object
+     */
+    private fun addMarkers() {
+
+        var places = ArrayList<Place>()
+
+        data.forEach{ case ->
+            var alert_level = 0
+            when (case[3]) {
+                "Tier 1" -> { alert_level = 1 }
+                "Tier 2" -> { alert_level = 2 }
+                "Tier 3" -> { alert_level = 3 }
+            }
+            places.add(Place(case[0], addressToLocation.getLocationFromAddress(this, case[0]), case[2], alert_level))
+        }
+
+        places.forEach{ place ->
+            when (place.alert_level) {
+                1 -> {
+                    val marker = mMap.addMarker(
+                        MarkerOptions()
+                            .title(place.name)
+                            .position(place.latLng)
+                            .icon(alertOneIcon)
+                            .visible(false)
+                    )
+                    marker.tag = place
+                    myMarkers.add(marker)
+                }
+                2 -> {
+                    val marker = mMap.addMarker(
+                        MarkerOptions()
+                            .title(place.name)
+                            .position(place.latLng)
+                            .icon(alertTwoIcon)
+                            .visible(false)
+                    )
+                    marker.tag = place
+                    myMarkers.add(marker)
+                }
+                3 -> {
+                    val marker = mMap.addMarker(
+                        MarkerOptions()
+                            .title(place.name)
+                            .position(place.latLng)
+                            .icon(alertThreeIcon)
+                            .visible(false)
+                    )
+                    // Set place as the tag on the marker object so it can be referenced within
+                    // MarkerInfoWindowAdapter
+                    marker.tag = place
+                    myMarkers.add(marker)
+                }
+
+            }
+        }
+    }
+
+    private fun mockData(): List<List<String>> {
+        //listOf("Site_title", "Site_state", "Site_address", "advice_title")
+        return listOf(
+            listOf(
+                "Hardrock Climbing | Melbourne CBD",
+                "VIC",
+                "123 test road",
+                "Tier 1"
+            ),
+            listOf(
+                "EQ Tower",
+                "VIC",
+                "123 test road",
+                "Tier 2"
+            ),
+            listOf(
+                "BIG W Queen Victoria Village",
+                "VIC",
+                "123 test road",
+                "Tier 3"
+            )
+        )
     }
 }
 
