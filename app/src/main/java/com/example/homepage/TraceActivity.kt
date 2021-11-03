7f31059435aefffc377406d83d94746d0c20daba
acryingship <43632113+acryingship@users.noreply.github.com>
2021-11-04 08:19:01 +1100

    Update TraceActivity.kt
    problems with null dates

--- app/src/main/java/com/example/homepage/TraceActivity.kt
+++ app/src/main/java/com/example/homepage/TraceActivity.kt
@@ -18,9 +18,15 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.homepage.databinding.ActivityMapsBinding
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.Marker
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
+import com.google.android.gms.maps.CameraUpdate
+
+
+
 
 class TraceActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -61,7 +67,7 @@ class TraceActivity : AppCompatActivity(), OnMapReadyCallback {
 
         val menuListener = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                val traceList = dataSnapshot.child("Trace").getValue()
+
                 traceDays = dataSnapshot //gets the location at different times of each day
                 Log.d("TD----------------:", "traceDays: " + traceDays.getValue())
 
@@ -80,7 +86,7 @@ class TraceActivity : AppCompatActivity(), OnMapReadyCallback {
             // Note that months are indexed from 0. So, 0 means January, 1 means february, 2 means march etc.
             val msg = "Selected date is " + dayOfMonth + "/" + (month + 1) + "/" + year
             Toast.makeText(this@TraceActivity, msg, Toast.LENGTH_SHORT).show()
-
+            mMap.clear()
             this.year = year.toString()
             this.month = month.toString()
             this.dayOfMonth = dayOfMonth.toString()
@@ -88,11 +94,46 @@ class TraceActivity : AppCompatActivity(), OnMapReadyCallback {
                 this.dayOfMonth = "0$dayOfMonth"
             }
             //read database
+
+            val days = traceDays.child(year.toString() + "-" + (month + 1).toString() + "-" + this.dayOfMonth)
             Log.d(
                 "onDateChangeListener",
-                "got" + traceDays.child(year.toString() + "-" + (month + 1).toString() + "-" + this.dayOfMonth)
+                "got   " + days
             )
+            if(days.toString()=="null"){
+                Toast.makeText(this@TraceActivity, "Theres no recorded trace master!EXITTTINGG", Toast.LENGTH_SHORT).show()
+                return@setOnDateChangeListener;
+            }
+            val markers = days!!.children // all the markers in a single day
 
+
+
+
+            var markerList:ArrayList<Marker> = ArrayList()
+
+            //add the markers at each (time, location) combo for each child
+            markers.forEach {
+                Log.d("Markers:",it.toString())
+                val strMarker = it.value.toString()
+                val Lng = strMarker.substringAfter("Lng=").substringBefore(",").toDouble()
+                val Lat = strMarker.substringAfter("Lat=").substringBefore("}").toDouble()
+                Log.d("LatLng:",Lat.toString()+","+Lng)
+                val pos = LatLng(Lat,Lng)
+                val marker = mMap.addMarker(MarkerOptions().position(pos).title(it.key.toString()))
+                //fitting all markers
+                markerList.add(marker);
+
+
+            }
+            //resizing to fit all markers
+            val b = LatLngBounds.Builder()
+            for (m in markerList) {
+                b.include(m.position)
+            }
+
+            val bounds = b.build()
+            val cu = CameraUpdateFactory.newLatLngBounds(bounds, 25, 25, 5)
+            mMap.animateCamera(cu)
         }
 
     }
