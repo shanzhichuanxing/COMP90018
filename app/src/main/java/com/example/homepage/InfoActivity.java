6e9af4195bf8a5dde2595fad759eeaa628b4132a
AmateurHackerWillBreakTheGame <leic5@student.unimelb.edu.au>
2021-11-05 16:48:11 +1100

    final version

--- app/src/main/java/com/example/homepage/InfoActivity.java
+++ app/src/main/java/com/example/homepage/InfoActivity.java
@@ -5,16 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.text.Html;
-import android.text.method.LinkMovementMethod;
 import android.view.View;
-import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class InfoActivity extends AppCompatActivity {
     private FloatingActionButton infoBack;
-    //TextView info_link = (TextView) findViewById(R.id.info_link);
     private View info_link;
 
     @Override
@@ -22,25 +18,16 @@ public class InfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
 
-       // info_link.setMovementMethod(LinkMovementMethod.getInstance());
         info_link = findViewById(R.id.info_link);
 
-        info_link.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent openURL = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.healthdirect.gov.au/symptom-checker/tool/basic-details"));
-                startActivity(openURL);
-            }
+        info_link.setOnClickListener(v -> {
+            Intent openURL = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.healthdirect.gov.au/symptom-checker/tool/basic-details"));
+            startActivity(openURL);
         });
 
 
        infoBack= findViewById(R.id.infoMenuBack);
-       infoBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(InfoActivity.this , MapsActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
-            }
-        });
+       infoBack.setOnClickListener(v -> startActivity(new Intent(InfoActivity.this , MapsActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)));
     }
 
 
